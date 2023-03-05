a00b6a2587728e5306e5bae205e2966e88c5277e
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-03-05 17:13:21 +0000

    Add metrics for request count

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,8 @@ dependencies {
     implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-server-apache")
     implementation("org.http4k:http4k-client-apache")
+    implementation("org.http4k:http4k-metrics-micrometer")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.10.4")
     testImplementation(kotlin("test"))
 }
 
--- app/src/main/kotlin/org/example/main.kt
+++ app/src/main/kotlin/org/example/main.kt
@@ -1,21 +1,36 @@
 package org.example
 
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
 import org.http4k.core.Method
 import org.http4k.core.Response
 import org.http4k.core.Status
+import org.http4k.core.then
+import org.http4k.filter.MicrometerMetrics
+import org.http4k.filter.ServerFilters
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.ApacheServer
 import org.http4k.server.asServer
 
 fun main() {
-    val app =
+    val metricsRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+
+    val routes =
         routes(
-            "/" bind Method.GET to { _ ->
-                println("Received request to /")
+            "/foo" bind Method.GET to { _ ->
                 Response(Status.OK).body("Hello")
+            },
+
+            "/metrics" bind Method.GET to { _ ->
+                Response(Status.OK).body(metricsRegistry.scrape())
             }
         )
+
+    val app =
+        ServerFilters
+            .MicrometerMetrics.RequestCounter(metricsRegistry)
+            .then(routes)
 
     println("Starting app")
 
