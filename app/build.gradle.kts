51b937bb892220d65f8ef751be87197538eea9a3
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-07-02 08:51:14 +0100

    Upgrade http4k to 5.2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.http4k:http4k-bom:4.41.1.1"))
+    implementation(platform("org.http4k:http4k-bom:5.2.1.0"))
     implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-server-apache")
     implementation("org.http4k:http4k-client-apache")
